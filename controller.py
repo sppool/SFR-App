ef52f48a7fde95074479808f10a51c1de94852f3
Pool <sppool.me98g@NCTU.edu.tw>
2022-05-31 20:42:09 +0800

    Re

--- controller.py
+++ controller.py
@@ -27,16 +27,14 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         self.ui.pushButtonChoiceFile.clicked.connect(self.OpenFile)
         self.ui.pushButtonCalculate.clicked.connect(self.Calculate)
         self.ui.pushButtonShowFourierFunc.clicked.connect(self.ShowFourierFunc)
-        self.ui.horizontalSliderFrequencyValue.valueChanged.connect(
-            self.getslidervalue)
+        self.ui.horizontalSliderFrequencyValue.valueChanged.connect(self.getslidervalue)
         self.ui.lineEditX.textEdited.connect(self.ReflashImage)
         self.ui.lineEditY.textEdited.connect(self.ReflashImage)
         self.ui.lineEditW.textEdited.connect(self.ReflashImage)
         self.ui.lineEditH.textEdited.connect(self.ReflashImage)
 
     def OpenFile(self):  # 選取文件
-        self.path, filetype = QFileDialog.getOpenFileName(
-            self, 'Open image', self.path, 'Images (*.png *.bmp *.jpg)')  # 開啟視窗名稱, 開啟資料夾, 種類Filter
+        self.path, filetype = QFileDialog.getOpenFileName(self, 'Open image', self.path, 'Images (*.png *.bmp *.jpg)')  # 開啟視窗名稱, 開啟資料夾, 種類Filter
         self.ui.lineEditFileName.setText(self.path)
         self.img = cv2.imread(self.path)  # 只讀取一次
         self.ReflashImage()  # 預設Show 出圖跟Roi
@@ -51,18 +49,14 @@ class MainWindow_controller(QtWidgets.QMainWindow):
     def ShowImageRefrash(self):
         if type(self.img) == np.ndarray:  # 確認影像有無
             img_show = self.img.copy()  # 要拿來畫圖的影像 copy 一份
-            cv2.rectangle(img_show, (self.roi_x, self.roi_y),
-                          (self.roi_x + self.roi_w, self.roi_y + self.roi_h), (0, 0, 255), 3)
-            cv2.putText(img_show, 'ROI', (self.roi_x, self.roi_y - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.8, (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.rectangle(img_show, (self.roi_x, self.roi_y), (self.roi_x + self.roi_w, self.roi_y + self.roi_h), (0, 0, 255), 3)
+            cv2.putText(img_show, 'ROI', (self.roi_x, self.roi_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.8, (0, 0, 255), 3, cv2.LINE_AA)
 
             height, width, channel = img_show.shape
             bytesPerline = channel * width
-            qimg = QImage(img_show, width, height,
-                          bytesPerline, QImage.Format_RGB888).rgbSwapped()
+            qimg = QImage(img_show, width, height, bytesPerline, QImage.Format_RGB888).rgbSwapped()
 
-            self.ui.labelImage.setPixmap(QPixmap.fromImage(
-                qimg).scaledToWidth(self.ui.labelImage.width()))
+            self.ui.labelImage.setPixmap(QPixmap.fromImage(qimg).scaledToWidth(self.ui.labelImage.width()))
             self.ui.labelImage.adjustSize()
             self.MsgRefresh('OK !!')
 
@@ -71,20 +65,16 @@ class MainWindow_controller(QtWidgets.QMainWindow):
 
     def ShowROIRefrash(self):
         if type(self.img) == np.ndarray:  # 確認影像有無
-            self.roi = self.img[self.roi_y:self.roi_y + self.roi_h,
-                                self.roi_x:self.roi_x + self.roi_w].copy()
+            self.roi = self.img[self.roi_y:self.roi_y + self.roi_h, self.roi_x:self.roi_x + self.roi_w].copy()
             height, width, channel = self.roi.shape
             bytesPerline = channel * width
-            qroi = QImage(self.roi, width, height,
-                          bytesPerline, QImage.Format_RGB888).rgbSwapped()
-            self.ui.labelROI.setPixmap(QPixmap.fromImage(
-                qroi).scaledToWidth(self.ui.labelROI.width()))
+            qroi = QImage(self.roi, width, height, bytesPerline, QImage.Format_RGB888).rgbSwapped()
+            self.ui.labelROI.setPixmap(QPixmap.fromImage(qroi).scaledToWidth(self.ui.labelROI.width()))
             self.ui.labelROI.adjustSize()
 
     def getslidervalue(self):  # 0 ~ 1 (step: 0.1)
         self.Freq = self.ui.horizontalSliderFrequencyValue.value() / 10
-        self.ui.labelFrequencyValue.setText(
-            f'Frequency: {self.Freq}')
+        self.ui.labelFrequencyValue.setText(f'Frequency: {self.Freq}')
         self.SFRRefresh()
 
     def Calculate(self):
@@ -95,6 +85,7 @@ class MainWindow_controller(QtWidgets.QMainWindow):
             self.SFRRefresh()
 
             return True
+
         else:
             self.MsgRefresh('ROI is Empty !!')
 
